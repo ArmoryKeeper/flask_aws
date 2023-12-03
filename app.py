a810744ed2ada0bd153964da3b7ed87be35cb082
Ivan Boskovski <boskovski648@gmail.com>
2023-12-03 16:02:37 +0100

    Basic Flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask  import Flask, request, jsonify
+from flask_restful import reqparse
+
+app = Flask(__name__)
+
+temps_put_args = reqparse.RequestParser()
+temps_put_args.add_argument('GPU Core' , type=str)
+temps_put_args.add_argument('GPU Fan' , type=str)
+temps_put_args.add_argument('CPU Package' , type=str)
+
+@app.route('/temps', methods=['PUT'])
+def temps():
+    args = temps_put_args.parse_args()
+    return jsonify(args), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
